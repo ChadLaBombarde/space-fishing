c8b528b5f6e5d6eafc86e740e521b78198165f2f
Chad LaBombarde <chadlabombarde@gmail.com>
2025-08-15 14:23:15 -0400

    update1.7

--- index.js
+++ index.js
@@ -12,11 +12,13 @@
         const articles = [
             {
                 title: 'The Art of Space Baiting',
-                content: 'In the vast expanse of space, traditional bait won\'t cut it. Discover how holographic lures and quantum attractants can help you reel in the biggest catches from the stars.'
+                content: 'In the vast expanse of space, traditional bait won\'t cut it. Discover how holographic lures and quantum attractants can help you reel in the biggest catches from the stars.',
+                image: "MICKFISH.png"
             },
             {
                 title: 'Top 10 Space Fishing Spots',
-                content: 'Explore our list of the best locations: 1. Asteroid Belt Alpha - Teeming with meteor minnows. 2. Black Hole Horizon - For the thrill-seekers chasing event horizon eels. And more!'
+                content: 'Explore our list of the best locations: 1. Asteroid Belt Alpha - Teeming with meteor minnows. 2. Black Hole Horizon - For the thrill-seekers chasing event horizon eels. And more!',
+                image: "MICK4FISHINGSPOT.png"
             }
         ];
 
@@ -26,6 +28,7 @@
             featured.innerHTML = `
                 <h3>${articles[index].title}</h3>
                 <p>${articles[index].content}</p>
+                <img src="${articles[index].image}" alt="${articles[index].title}" style="max-width: 100%; height: auto;">
             `;
         }
 
