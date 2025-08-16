8dab0d2b4adaf0b85bdccb6b82c8e1174f921dac
Chad LaBombarde <chadlabombarde@gmail.com>
2025-08-16 09:22:28 -0400

    update2.1test

--- index.js
+++ index.js
@@ -34,20 +34,4 @@
 
         
 // Generate random bubbles
-    function createBubbles() {
-      const bubblesContainer = document.querySelector('.bubbles');
-      if (bubblesContainer.children.length > 0) {
-    return; }
-      for (let i = 0; i < 70; i++) {
-        const bubble = document.createElement('div');
-        bubble.classList.add('bubble');
-        bubble.style.left = `${Math.random() * 100}%`;
-        bubble.style.width = `${Math.random() * 15 + 5}px`;
-        bubble.style.height = bubble.style.width;
-        bubble.style.animationDuration = `${Math.random() * 5 + 5}s`;
-        bubble.style.animationDelay = `${Math.random() * 3}s`;
-        bubblesContainer.appendChild(bubble);
-      }
-    }
-    // Run only once on page load
-document.addEventListener('DOMContentLoaded', createBubbles, { once: true });
+    
